243c971a0e61e3679cb4f735eb13e8cda7dd0f71
lowel <10vvvvvv31@gmail.com>
2022-04-04 13:29:20 +0300

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+$connect = new PDO('mysql:host=localhost;dbname=address_info', 'user', 'password');
+$connect->prepare('INSERT INTO data (message) VALUES (?)')->execute(['хахахаха попробуй вот это щенок']);
+$statement = $connect->query('SELECT * FROM data');
+$row = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo var_dump($row);
