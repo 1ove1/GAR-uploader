95f4874d29575f112430dabe686cff191ede3afb
lowel <10vvvvvv31@gmail.com>
2022-04-04 16:16:24 +0300

    create simple db-singleton-facade for connection with bd

---
+++ src/DBFactory/DBFacade.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace TPO\LAB2\DBFactory;
+
+use TPO\LAB2\Env;
+
+/**
+ * 	Some special funciton for bd connection here
+ */
+class DBFacade
+{
+	static ?PDO $instance = null;
+
+	/**
+	 *  Get curr instance of database
+	 * @return PDO 	PDO-object with curr db connection
+	 */
+	public static function getInstance() {
+		
+		if (self::$instance === null) {
+			self::$instance = self::connect();
+
+		} 
+
+		return self::$instance;
+	}
+
+	public static function connect() : PDO
+	{
+		$db_type = Env::db_type->value;
+		$host = Env::host->value;
+		$db = Env::db->value;
+		$user = Env::user->value;
+		$pass = Env::pass->value;
+
+		try {
+			$PDO = new PDO($db_type . ':host=' . $host . ';dbname=' . $db, $user, $pass);
+		} catch (PDOException $exception) {
+			echo $exception->getMessage() . $exception->getCode();
+		}
+		
+		return $PDO;
+	}
+}
