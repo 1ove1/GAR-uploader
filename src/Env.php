1b631c067d14115dfe06379001d27e14004988db
lowel <10vvvvvv31@gmail.com>
2022-04-10 16:17:52 +0300

    add paths to log and cache floder

--- src/Env.php
+++ src/Env.php
@@ -2,12 +2,19 @@
 
 namespace LAB2;
 
+
+/**
+ * ENVOIRMENT ENUM
+ *
+ * CONTAINS ALL NEEDED INFORMATION ABOUT DB AND PATHS
+ */
 enum Env : string {
-	case db_type 	= "mysql";
+	case db_type  	= "mysql";
 	case db 		= "address_info";
 	case host 		= "localhost";
 	case user 		= "user";
 	case pass 		= "password";
 	case zipPath  	= __DIR__ . "/gar_delta_xml.zip";
-	case cachePath	= __DIR__ . "/cache";
+	case cachePath	= __DIR__ . "/.cache";
+	case logPath	= __DIR__ . "/.logs";
 }
