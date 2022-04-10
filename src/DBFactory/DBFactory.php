9d183c60f4e778b027df6f9d8e3c39e5a6a71f83
lowel <10vvvvvv31@gmail.com>
2022-04-10 16:20:01 +0300

    add comments

--- src/DBFactory/DBFactory.php
+++ src/DBFactory/DBFactory.php
@@ -5,8 +5,18 @@ namespace LAB2\DBFactory;
 use LAB2\DBFactory\DBFacade;
 use LAB2\DBFactory\Tables\AddressInfo;
 
+/**
+ * BDFACTORY CLASS
+ *
+ * FULL-STATIC FABRIC
+ * RETURN COMPLETED MODELS
+ */
 final class DBFactory 
 {
+	/**
+	 * return address info table
+	 * @return void
+	 */
 	public static function getAddressInfoTable()
 	{
 		return new AddressInfo(DBFacade::getInstance());
