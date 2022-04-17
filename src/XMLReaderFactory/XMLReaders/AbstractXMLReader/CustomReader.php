aafd0d458d998aab73f77f1216cb841711d3cf02
lowel <10vvvvvv31@gmail.com>
2022-04-17 23:38:33 +0300

    custom reader interface

---
+++ src/XMLReaderFactory/XMLReaders/AbstractXMLReader/CustomReader.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace LAB2\XMLReaderFactory\XMLReaders\AbstractXMLReader;
+
+/**
+ * CUSTOMREADER INTERFACE
+ *
+ * DEFINES METHODS THAT COULD USE FOR
+ * GETTING INFO ABOUT ELEMS AND ATTRIBUTES THAT
+ * SHOULD BE PARSE
+ */
+interface CustomReader
+{
+	/**
+	 * return elements of xml document
+	 * @return array elements names
+	 */
+	static function getElements() : array;
+
+	/**
+	 * return attributes of elements in xml document
+	 * @return array attributes names
+	 */
+	static function getAttributes() : array;
+}
