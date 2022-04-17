ff6a7c8609f2cd3e8db5c865e35cd7412fad35c7
lowel <10vvvvvv31@gmail.com>
2022-04-17 23:44:35 +0300

    simple examples

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$connect = new PDO('mysql:host=localhost;dbname=address_info', 'user', 'password');
-$connect->prepare('INSERT INTO data (id, message) VALUES (:id, :message)')->execute(['message' => 'хахахаха попробуй вот это щенок']);
-$statement = $connect->query('SELECT * FROM data');
-$row = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo var_dump($row);
+use LAB2\XMLReaderFactory\XMLReaderFactory;
+use LAB2\DBFactory\DBFactory;
+
+// DBFactory::getAddressInfoTable();
+
+XMLReaderFactory::excecAddrObj();
+
+// \LAB2\Log::write('enter to database...');
+// $connect = new PDO('mysql:host=localhost;dbname=address_info', 'user', 'password');
+// \LAB2\Log::write('connected!');
+
+// \LAB2\Log::write('making query...');
+// $connect->prepare('INSERT INTO tests (message) VALUES (:message)')->execute(['message' => 'хахахаха попробуй вот это щенок']);
+// $statement = $connect->query('SELECT * FROM tests');
+
+// \LAB2\Log::write('parse result...');
+// $row = $statement->fetchAll(PDO::FETCH_ASSOC);
+
