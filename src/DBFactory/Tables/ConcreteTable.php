da6d1996587ec51b07bb9bec5c75461fb5d3caf9
lowel <10vvvvvv31@gmail.com>
2022-04-17 23:46:29 +0300

    create table context

--- src/DBFactory/Tables/ConcreteTable.php
+++ src/DBFactory/Tables/ConcreteTable.php
@@ -5,7 +5,7 @@ namespace LAB2\DBFactory\Tables;
 use LAB2\DBFactory\Tables\AbstractTable\{
 	AbstractTable, Queries, MetaTable
 };
-
+use LAB2\DBFactory\Tables\CreateTable;
 use LAB2\{Log, Msg};
 
 
@@ -15,7 +15,7 @@ use LAB2\{Log, Msg};
  * IMPLEMENTS ABSTRACTTABLE METHODS 
  * (OR MODIFIED THEM)
  */
-abstract class ConcreteTable extends AbstractTable
+abstract class ConcreteTable extends AbstractTable implements CreateTable
 {
 	use Queries, MetaTable;
 
@@ -25,17 +25,100 @@ abstract class ConcreteTable extends AbstractTable
 	 */
 	function __construct(\PDO $connection) {
 		try {
+			if (!empty($this->getFieldsToCreate())) { 
+				$createFields = $this->getFieldsToCreate();
+				$createTableName = $this->getTableName(get_class($this));
+
+				$this->createTable(
+					$connection,
+					$createTableName,
+					$createFields
+				);
+			}
 			parent::__construct($connection);
 			
-			Log::write(sprintf(
-				"%s %s %s",
+			Log::write(
 				Msg::LOG_DB_TABLE->value, 
 				$this->name, 
 				Msg::LOG_COMPLETE->value
-			));
+			);
 		} catch (PDOException $excp) {
 			Log::error($excp);
-		} 
+		}
 	}
 
+	/**
+	 * default implementation of method
+	 * @return array empty
+	 */
+	public function getFieldsToCreate() : array
+	{
+		return [];
+	}
+
+	/**
+	 * create table method
+	 * @param  \PDO   $connection     connected PDO object
+	 * @param  string $nameOfTable    table name
+	 * @param  array  $fieldsToCreate fields to create
+	 * @return void
+	 */
+	public function createTable(\PDO $connection, string $nameOfTable, array $fieldsToCreate) : void
+	{
+		if (!$this->tableDropConfirmIfExists($connection, $nameOfTable)) {
+			return;
+		}
+
+
+		Log::write(
+			Msg::LOG_DB_CREATE->value, $nameOfTable,
+			Msg::LOG_DB_WITH_FIELDS->value, 
+			implode(', ', array_keys($fieldsToCreate))
+		);
+
+		$formatedFields = [];
+
+		foreach ($fieldsToCreate as $field => $params) {
+			if (empty($params)) {
+				throw new \InvalidArgumentException(sprintf(
+					"field %s should contains type params!",
+					$field
+				));
+			}
+
+			$formatedFields[] = sprintf(
+				"%s %s",
+				$field,
+				implode(' ', $params)
+			);
+		}
+
+		$createQuery = sprintf(
+			"DROP TABLE IF EXISTS %s; CREATE TABLE %s (%s)", 
+			$nameOfTable,
+			$nameOfTable,
+			implode(', ', $formatedFields) 
+			);
+ 
+		$connection->exec($createQuery);
+	}
+
+	private function tableDropConfirmIfExists(\PDO $connection, string $nameOfTable) : bool
+	{
+		$tableList = $connection->query("SHOW TABLES")->fetchAll(\PDO::FETCH_COLUMN);
+		
+		if (in_array($nameOfTable, $tableList)) {
+			do {
+				$confirm = readline(
+					$nameOfTable . Msg::LOG_DB_DROP_CONFIRM->value
+				);
+			} while(!preg_match('/[YyNnДдНн]+/', $confirm));
+			
+			if (preg_match("/[NnНн]/", $confirm)) {
+				return FALSE;
+			} 
+		}
+
+		return TRUE;
+	}
 }
