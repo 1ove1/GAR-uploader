9197e781af06e5fb36cfaf7a4996695444b2df8d
lowel <10vvvvvv31@gmail.com>
2022-04-17 23:47:51 +0300

    add comments

--- src/XMLReaderFactory/XMLReaders/AbstractXMLReader/OpenXMLFromZip.php
+++ src/XMLReaderFactory/XMLReaders/AbstractXMLReader/OpenXMLFromZip.php
@@ -8,32 +8,49 @@ trait OpenXMLFromZip
 	/**
 	 *  Extracting concrete file from zip archive into temp floder
 	 * @param  string $pathToZip  path to zip archive
-	 * @param  string $fileName   name of file or path in zip
-	 * @param  string $saveFloder path to temp floder
-	 * @return string             return full path to extract file
+	 * @param  string $fileName   name of file or name like /[A-Za-z_-\/]+[0-9]/
+	 * @param  string $cachePath  apath to temp floder
+	 * @return string             return absolute path to extract file
 	 */
 	public function extractFileFromZip(string $pathToZip, 
 									   string $fileName, 
-									   string $saveFloder) : string
+									   string $cachePath) : string
 	{
 		$zip = new \ZipArchive;
+
 		if ($zip->open($pathToZip) === TRUE) {
-			$zip->extractTo($saveFloder, $fileName);
+
+			for ($iter = 0; $iter < $zip->numFiles; ++$iter)
+			{
+				$realName = $zip->getNameIndex($iter);
+
+				if ($fileName === $realName) 
+				{
+					break;
+				}
+
+				if (preg_match("/" . implode("\/", explode("/", $fileName)) . "_[0-9]+/", $realName)) {
+					$fileName = $realName;
+					break;
+				}
+			}
+			$zip->extractTo($cachePath, $fileName);
 			$zip->close();
 		} else {
-			throw Exception('invalid operation: open xml file ' . 
+			throw new \Exception(__DIR__ . PHP_EOL . 'invalid operation: open xml file ' . 
 							$fileName . ' from ' . $pathToZip);
 		}
-		return $saveFloder . $fileName;
+
+		return $cachePath . '/' . $fileName;
 	}
 
 	/**
 	 *  Method for open xml files from the path param
-	 * @param  string $pathToFile path to the concrete xml file
+	 * @param  string $pathToXml  path to the concrete xml file
 	 * @return \XMLReader         XMLReader object
 	 */
-	public function openXML(string $pathToFile) : \XMLReader
+	public function openXML(string $pathToXml) : \XMLReader
 	{
-		return \XMLReader::open($pathToFile);
+		return \XMLReader::open($pathToXml);
 	}
 }
