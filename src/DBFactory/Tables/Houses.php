93e50a01a43efb2f0edcea05b5e5348d3c65896b
lowel <10vvvvvv31@gmail.com>
2022-04-17 23:35:46 +0300

    houses object

---
+++ src/DBFactory/Tables/Houses.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace LAB2\DBFactory\Tables;
+
+use LAB2\DBFactory\Tables\ConcreteTable;
+
+
+/**
+ * AS HOUSES CLASS-MODEL
+ *
+ * EXTENDS CONCRETE TABLE AND USING FOR COMMUNICATE
+ * WITH TABLE 'address_info'
+ */
+class Houses extends ConcreteTable 
+{
+	public function getFieldsToCreate() : array 
+	{
+		return [
+			'id_houses' => [
+				'INTEGER', 
+				'auto_increment', 
+				'PRIMARY KEY',
+			],
+			'id' => [
+				'CHAR(50)',
+			],
+			'objectid' => [
+				'CHAR(50)',
+			],
+			'objectguid' => [
+				'CHAR(50)',
+			],
+			'housenum' => [
+				'CHAR(100)',
+			],
+			'housetype' => [
+				'CHAR(50)',
+			],
+		];
+	}
+}
